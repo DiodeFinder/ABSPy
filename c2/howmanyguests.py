4eec984de3b27b12515d6c935819a7ea094de767
samsreal <bsaumyaranjan95@gmail.com>
2022-04-01 20:37:27 +0530

    added ssh key

---
+++ c2/howmanyguests.py
@@ -0,0 +1,9 @@
+name = ''
+while not name:
+	print('Enter your name: ')
+	name = input()
+print('How many guest will have?')
+numOfguest = int(input())
+if numOfguest:
+	print('Be sure to have room for all your guests ?')
+print('Done')
---
+++ c2/oneTohundred.py
@@ -0,0 +1,4 @@
+total = 0
+for num in range(101):
+	total += num
+print('The sum of number from 1 to 100 is ' + str(total) + '.')
